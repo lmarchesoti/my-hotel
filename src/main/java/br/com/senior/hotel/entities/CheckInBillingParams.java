8c61ef4a622f35d229450ff405b605ce074b0f43
Lucas Marchesoti Franco <lmarchesoti@hotmail.com>
2024-08-06 14:56:19 -0300

    Add CheckInBillingParams

---
+++ src/main/java/br/com/senior/hotel/entities/CheckInBillingParams.java
@@ -0,0 +1,39 @@
+package br.com.senior.hotel.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckInBillingParams {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "DIARIA_SEG_A_SEX")
+    private Double diariaSegASex;
+
+    @Column(name = "DIARIA_FIM_DE_SEMANA")
+    private Double diariaFimDeSemana;
+
+    @Column(name = "GARAGEM_SEG_A_SEX")
+    private Double garagemSegASex;
+
+    @Column(name = "GARAGEM_FIM_DE_SEMANA")
+    private Double garagemFimDeSemana;
+
+    @Column(name = "HORARIO_LIMITE_CHECKOUT")
+    private LocalTime horarioLimiteCheckout;
+}
